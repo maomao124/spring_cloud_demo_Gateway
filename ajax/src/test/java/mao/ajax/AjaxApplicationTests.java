71edd8318bf0385574bd6ae253885b9b6a41fda2
mao <1296193245@qq.com>
2022-07-18 19:25:44 +0800

    spring_cloud_demo_Gateway

---
+++ ajax/src/test/java/mao/ajax/AjaxApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
